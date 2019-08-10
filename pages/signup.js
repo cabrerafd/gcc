ecc4a8d2486e1a9aade64f27c43774e10aaa9b7e
crimsonred005 <cabrerafp005@gmail.com>
2019-08-10 15:03:46 +0800

    Disabled Sign up button

--- pages/signup.js
+++ pages/signup.js
@@ -212,7 +212,7 @@ export default function SignUp() {
             variant='contained'
             color='primary'
             className={classes.submit}
-            disabled={loading}
+            disabled={true}
           >
             Sign Up
             {loading && (
