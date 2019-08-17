816752ad63d59722b8a8324d91edff115f3c218a
crimsonred005 <cabrerafp005@gmail.com>
2019-08-17 19:18:28 +0800

    Changed Baas to firebase

---
+++ src/firebase.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app'
+import 'firebase/database'
+import 'firebase/auth'
+// Your web app's Firebase configuration
+
+const firebaseConfig = {
+  apiKey: process.env.apiKey,
+  authDomain: process.env.authDomain,
+  databaseURL: process.env.databaseURL,
+  projectId: process.env.projectId,
+  storageBucket: '',
+  messagingSenderId: process.env.messagingSenderId,
+  appId: process.env.messagingSenderId,
+}
+// Initialize Firebase
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig)
+}
+
+export default firebase
